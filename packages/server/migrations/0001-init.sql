64b1ff79cff8da0c7c0dd89733177c9a94dedd7e
Andrew Berry <andrew.berry@outlook.com>
2021-08-21 21:52:16 +1000

    db migrations

--- packages/server/migrations/0001-init.sql
+++ packages/server/migrations/0001-init.sql
@@ -1,7 +1,7 @@
 CREATE TYPE acc_type AS ENUM ('uim', 'hcim', 'im', 'reg');
 
 CREATE TABLE stats_pit (
-  stats_id UUID DEFAULT uuid_generate_v4(),
+  stats_id SERIAL,
   created_at timestamp DEFAULT now(),
   is_latest boolean DEFAULT TRUE,
   
@@ -13,19 +13,13 @@ CREATE TABLE stats_pit (
   total_lvl decimal DEFAULT 0,
   total_rnk decimal DEFAULT 0,
   
-  smithing_exp decimal DEFAULT 0,
-  smithing_ehp decimal DEFAULT 0,
-  smithing_lvl decimal DEFAULT 0,
-  smithing_rnk decimal DEFAULT 0,
-  
   primary key (stats_id)
 );
 
 CREATE VIEW ranks AS
   SELECT
     ign,
-    DENSE_RANK() OVER (ORDER BY total_ehp DESC) AS total_rnk,
-    DENSE_RANK() OVER (ORDER BY smithing_ehp DESC) AS smithing_rnk
+    DENSE_RANK() OVER (ORDER BY total_ehp DESC) AS total_rnk
   FROM stats_pit
   WHERE
     is_latest IS TRUE;
--- packages/server/src/migrate.ts
+++ packages/server/src/migrate.ts
@@ -1,15 +1,19 @@
+import * as path from "path";
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 import { migrate } from "postgres-migrations";
 
-import { databaseLoader } from "./loaders/databaseLoader";
 import { dbPool } from "./utils/dbPool";
 
 async function execMigrations() {
-  await databaseLoader();
-  migrate({ client: dbPool }, "/migrations");
+  await migrate(
+    { client: dbPool },
+    path.resolve(__filename, "../../migrations")
+  );
+  await dbPool.end();
 }
 
 execMigrations();
